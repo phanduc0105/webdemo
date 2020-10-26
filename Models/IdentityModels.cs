dfa1d2e7abbc713fa69425cf369ca2aa5d216fbc
phanduc0105 <phanduc0105@gmail.com>
2020-10-26 14:49:21 +0700

    fix net farmework

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace webdemo.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("webdemo_Model", throwIfV1Schema: false)
         {
         }
 
